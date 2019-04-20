3e3b7e360cb9f694393069fbbc99ca3a06d1ca14
Kevin <moturikev@gmail.com>
2019-04-20 19:17:30 +0300

    Designing Portfolio Landing Page

--- src/App.js
+++ src/App.js
@@ -1,28 +1,126 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+let index = 0;
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+
+  next = () => {
+
+      if(index < 5) {
+
+          const sections = document.querySelectorAll('section');
+
+          index++;
+
+          sections.forEach((section, i) => {
+
+              if(i === index) {
+
+                  section.scrollIntoView({behavior: 'smooth'});
+
+              }
+              
+          });
+
+      }
+
   }
+
+  prev = () => {
+
+      if(index > 0) {
+
+          const sections = document.querySelectorAll('section');
+
+          index--;
+
+          sections.forEach((section, i) => {
+
+              if(i === index) {
+
+                  section.scrollIntoView({behavior: 'smooth'});
+
+              }
+              
+          });
+
+      }
+
+  }
+
+  render() {
+
+    return (
+
+      <div className="main">
+
+        <div className="content">
+        
+          <section>
+
+            <div className="overlay"></div>
+
+            <div className="wrapper">
+
+              <div className="intro">
+
+                <h1>Hi, I`m Kevin</h1>
+
+                <p>Full Stack Software Developer</p>
+
+              </div>
+
+            </div>
+          
+          </section>
+        
+          <section>
+          
+            <h1>Section 2</h1>
+          
+          </section>
+        
+          <section>
+          
+            <h1>Section 3</h1>
+          
+          </section>
+        
+          <section>
+          
+            <h1>Section 4</h1>
+          
+          </section>
+        
+          <section>
+          
+            <h1>Section 5</h1>
+          
+          </section>
+        
+          <section>
+          
+            <h1>Section 6</h1>
+          
+          </section>
+        
+        </div>
+
+        <div className="nav">
+
+          <button className="prev" onClick={() => this.prev()} id="prev">Previous</button>
+
+          <button className="next" onClick={() => this.next()} id="next">Next</button>
+
+        </div>
+        
+      </div>
+
+    );
+
+  }
+
 }
 
 export default App;
