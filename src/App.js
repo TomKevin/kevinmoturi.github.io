e97dcad7342b891410472070288a3f1c381ed76a
Kevin <moturikev@gmail.com>
2019-08-01 15:00:13 +0300

    Desigining Technology Stack Images Slider

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
 import React, { Component } from 'react';
+import Carousel, { Dots } from '@brainhubeu/react-carousel';
+import '@brainhubeu/react-carousel/lib/style.css';
 import './App.css';
+
+const ImageWrapper = ({ source, title }) => (
+
+    <figure>
+
+      <img src={source} alt={title} />
+
+      <figcaption className="caption">
+
+       {title}
+
+      </figcaption>
+
+    </figure>
+
+);
+
 
 let index = 0;
 
 class App extends Component {
+
+  constructor(props) {
+
+    super(props);
+
+    this.state = { 
+      
+      value: 0 
+      
+    };
+    
+  }
+  
+  onChange = (value) => {
+
+    this.setState({ value });
+
+  }
 
   next = () => {
 
@@ -49,6 +86,8 @@ class App extends Component {
 
   }
 
+  handleOnDragStart = e => e.preventDefault();
+
   render() {
 
     return (
@@ -67,18 +106,70 @@ class App extends Component {
 
                 <h1>Hi, I`m Kevin</h1>
 
-                <p>Full Stack Software Developer</p>
+                <p>Software Developer</p>
 
               </div>
 
             </div>
           
           </section>
-        
+
           <section>
-          
-            <h1>Section 2</h1>
-          
+
+            <h1>My Technology Stack</h1>
+
+            <Carousel
+
+              slidesPerPage={3}
+
+              centered
+
+              value={this.state.value}
+
+              onChange={this.onChange}
+
+              slides={[
+
+                (<ImageWrapper source={require('./assets/img/react.png')} title="React Js And React Native" />),
+
+                (<ImageWrapper source={require('./assets/img/nodejs.png')} title="Node Js" />),
+
+                (<ImageWrapper source={require('./assets/img/mongodb.png')} title="Mongo DB" />),
+
+                (<ImageWrapper source={require('./assets/img/graphql.png')} title="GraphQL" />),
+
+                (<ImageWrapper source={require('./assets/img/laravel.png')} title="Laravel" />),
+
+                (<ImageWrapper source={require('./assets/img/electron.png')} title="Electron" />),
+
+                (<ImageWrapper source={require('./assets/img/mysql.png')} title="MySQL" />),
+
+              ]}
+
+              autoPlay={2000}
+
+              animationSpeed={1000}
+
+              infinite
+              
+              breakpoints={{
+
+                1000: {
+
+                  slidesPerPage: 2,
+
+                },
+
+                500: {
+
+                  slidesPerPage: 1,
+
+                }
+                
+              }}
+
+            />
+
           </section>
         
           <section>
